677caf18f6148c58f09b879cdd013aaaa25123bb
Sam Mulqueen <sam.mulqueen.nz@gmail.com>
2018-07-08 01:03:03 +1200

    Kinda got it working, looks like an arrow

--- tree-fractal.js
+++ tree-fractal.js
@@ -1,5 +1,5 @@
 var settings = {
-    width: 700,
+    width: 500,
     height: 500
 };
 
@@ -8,14 +8,22 @@ function setup() {
 }
 
 function draw() {
-    branch(3, 0, 0, 0, 1);
+    translate(settings.width/2, settings.height/2);
+    branch(3, 0, 0, 0, 250);
 }
 
 function branch(iter, x , y, angle, scale)
 {
     if(iter>0)
-    {
-        translate(settings.width/2, settings.height/2);
+    {   
+        var newX = scale*Math.cos(angle);
+        var newY = scale*Math.sin(angle);
+        var newScale = scale * 0.8;
+        var angle1 = angle - 0.2;
+        var angle2 = angle + 0.2;
+        line(x, y, newX, newY);
+        branch(iter-1, newX, newY, angle1, newScale);
+        branch(iter-1, newX, newY, angle2, newScale);
         //
     }
 }
